97df9de3d7d21b36e42741be37a01cf341dc0dea
Dmytro Piestrietsov <ki23-d.piestrietsov@nubip.edu.ua>
2025-12-19 06:14:19 +0200

    Solved fetch lab

--- tasks/task1.js
+++ tasks/task1.js
@@ -1,13 +1,11 @@
-"Ваша функція повинна робити GET-запит до вказаного URL і отримати дані."
-"Поверніть дані користувачів у форматі масиву"
-"Дані мають включати такі поля, як id та name."
+async function fetchUsers() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
 
-"https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
-
-function fetchUsers() {
-  // Ваш код
+  return data.map(user => ({
+    id: user.id,
+    name: user.name
+  }));
 }
-
-console.log(fetchUsers())
 
 module.exports = fetchUsers;
--- tasks/task4.js
+++ tasks/task4.js
@@ -1,13 +1,12 @@
-"Ваш код повинен зробити DELETE-запит до вказаного URL, де {userId} – це ID користувача, якого потрібно видалити."
-"Поверніть статус відповіді сервера після видалення."
+async function deleteUser(id) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`,
+    {
+      method: 'DELETE',
+    }
+  );
 
-"https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
-
-
-function deleteUser(id) {
-  // Ваш код
+  return response;
 }
-
-console.log(deleteUser(1));
 
 module.exports = deleteUser;
